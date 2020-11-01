60692b4995ccc1492eb6165c2629420987ea7159
heesangs_mac <heesangs@heesangs-macui-MacBookPro.local>
2020-11-01 14:29:11 +0900

    AddPlayerForm onChange

--- src/component/AddPlayerForm.jsx
+++ src/component/AddPlayerForm.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 class AddPlayerForm extends React.Component {
-  render() {
-    return(
-      <form className="form">
-        <input type="text" className="input" placeholder="enter a player's name"/>
-        <input type="submit" className="input" value="Add Player"/>
-      </form>
-    )
-  }
+    state = {
+        value: ""
+    }
+
+    handleChangeValue = (e) => {
+        this.setState({ value: e.target.value })
+    }
+
+    render() {
+        return (
+            <form className="form">
+                <input type="text" className="input" placeholder="enter a player's name"
+                    value={this.state.value}
+                    onChange={this.handleChangeValue}
+                />
+                <input type="submit" className="input" value="Add Player" />
+            </form>
+        )
+    }
 }
+
 export default AddPlayerForm; 
