e5705840d65c099c2d64071b338290b9dad1e6c2
heesangs_mac <heesangs@heesangs-macui-MacBookPro.local>
2020-11-01 19:06:31 +0900

    Stopwatch UI

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Statistics from "./Statistics"
+import Stopwatch from "./Stopwatch"
 
 export const Header = (props) => {
   return (
     <header className='header'>
       <h1 className="h1">{props.title}</h1>
       <Statistics players={props.players}></Statistics>
+      <Stopwatch></Stopwatch>
     </header>
   );
 }
